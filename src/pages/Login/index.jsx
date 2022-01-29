cd9e064ce473f18bf0372d52689b04f447c6ab9f
Awen <2889984509@qq.com>
2022-01-29 19:27:19 +0800

    antd库

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
-
+import { Button } from "antd";
 export default class Login extends Component {
   render() {
-    return <div>Login Component</div>;
+    return (
+      <div>
+        Login Component
+        <Button type="primary">Primary Button</Button>
+      </div>
+    );
   }
 }
