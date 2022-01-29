e6f540001cf9e91d6b49572ca335e35745213811
Awen <2889984509@qq.com>
2022-01-29 19:13:04 +0800

    配置路由

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  NavLink,
+  Navigate,
+} from "react-router-dom";
+import Layout from "./pages/Layout";
+import Login from "./pages/Login";
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <Router>
+      <div className="App">
+        {/* <NavLink to="/login">登录</NavLink>
+        <NavLink to="/home">首页</NavLink> */}
+
+        {/* 配置路由规则 */}
+        <Routes>
+          <Route path="/home" element={<Layout></Layout>}></Route>
+          <Route path="/login" element={<Login></Login>}></Route>
+          {/* <Route path="/*" element={<Navigate to="/home" />}></Route> */}
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
